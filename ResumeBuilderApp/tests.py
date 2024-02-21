b9cd7668d5ee5dea74a35b6873e12f2728b029f8
nchaimlo <123335216+nchaimlo@users.noreply.github.com>
2024-02-21 22:00:56 +0000

    combined to run

--- ResumeBuilderApp/tests.py
+++ ResumeBuilderApp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Skill, Languages, Interests, MyUser, Education, WorkExperience, Projects, CertificationsAndAwards
+from datetime import date
+
+class TestMyUser(TestCase):
+  def setUp(self):
+    self.user = MyUser.objects.create_user(username='testuser',  
+                                           email='email@example.com', 
+                                           password='testpassword',
+                                           first_name='test',
+                                           last_name='name',
+      )
+
+  def test_user_creation(self):
+    self.assertEqual(MyUser.objects.count(), 1)
+
 
 # Create your tests here.
