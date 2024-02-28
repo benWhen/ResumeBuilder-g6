a6df464c77675c900d89ce12382f097e8641d2a3
nchaimlo <chaimlo2@uwm.edu>
2024-02-28 15:04:55 -0600

    Can now logout

--- ResumeBuilderApp/views.py
+++ ResumeBuilderApp/views.py
@@ -8,6 +8,10 @@ def home(request):
     return render(request, 'pages/home.html')
 
 
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
+
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -15,8 +19,12 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
-    return render(request, 'pages/login.html')
+            return redirect('dashboard')
+        else:
+            messages.info(request, 'Username or password is incorrect.')
+            return render(request, 'pages/login.html')
+    context = {}
+    return render(request, 'pages/login.html', context)
 
 
 def register(request):
@@ -30,3 +38,8 @@ def register(request):
             return redirect('login')
     context = {'form': form}
     return render(request, 'pages/register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
