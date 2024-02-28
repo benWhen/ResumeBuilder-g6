a600afa88e15c06ee30e8748e35303c51e675521
nchaimlo <chaimlo2@uwm.edu>
2024-02-28 02:12:24 -0600

    CAN FINALLY CREATE USERS FROM REGISTER.

--- ResumeBuilderApp/views.py
+++ ResumeBuilderApp/views.py
@@ -1,51 +1,23 @@
-from django.contrib import messages
-from django.shortcuts import render, redirect
-from .forms import MyUserCreationForm, MyAuthenticationForm
-from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-
-# Create your views here.
-def user_login(request):
-    if request.method == 'POST':
-        form = MyAuthenticationForm(request, request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, email=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                form.add_error(None, 'Invalid email or password.')
-    else:
-        form = MyAuthenticationForm()
-    return render(request, 'pages/login.html', {'form': form})
-
-
-def register(request):
-    if request.method == 'POST':
-        print("Register view function is being executed!")
-        form = MyUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(email=email, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = MyUserCreationForm()
-    return render(request, 'pages/register.html', {'form': form})
-
-
-def user_logout(request):
-    logout(request)
-    return redirect('pages/login.html')
+from .forms import CreateUserForm
 
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def user_login(request):
+    return render(request, 'pages/login.html')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'pages/register.html', context)
--- django_project/urls.py
+++ django_project/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from ResumeBuilderApp import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('login/', views.user_login, name='login'),
-    path('register/', views.register, name='register'),
-    path('home/', views.home, name='home')
+    path('', include('ResumeBuilderApp.urls')),
 ]
