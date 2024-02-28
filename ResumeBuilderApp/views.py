ff79a84606ca1fefbaa5afad5b2c3ac0e67dd1f2
nchaimlo <chaimlo2@uwm.edu>
2024-02-28 14:21:07 -0600

    Users can now login and register

--- ResumeBuilderApp/views.py
+++ ResumeBuilderApp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
@@ -8,16 +9,24 @@ def home(request):
 
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
     return render(request, 'pages/login.html')
 
 
 def register(request):
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
     context = {'form': form}
     return render(request, 'pages/register.html', context)
