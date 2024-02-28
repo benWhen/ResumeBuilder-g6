26c22f6232ec83fe50e9563ebebba83df6cafbde
nchaimlo <chaimlo2@uwm.edu>
2024-02-28 14:21:48 -0600

    urls

---
+++ ResumeBuilderApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
+    ]
