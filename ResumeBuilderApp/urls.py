2042e74dcef713ece4780cc491e5d278cd3911cc
PvtWebster <65383191+PvtWebster@users.noreply.github.com>
2024-04-10 17:06:43 -0500

    Update urls.py
    Signed-off-by: PvtWebster <65383191+PvtWebster@users.noreply.github.com>

--- ResumeBuilderApp/urls.py
+++ ResumeBuilderApp/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('editor/', views.editor, name='editor'),
     path('saveResume/', views.saveResume, name='saveResume'),
-    path('quickResume/<int:user_id>', views.quickResume, name='makeResume')
+    path('quickResume/<int:user_id>', views.quickResume, name='quickResume')
     ]
 #comment so I can re-push
