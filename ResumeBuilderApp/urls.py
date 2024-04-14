27b0374357b9d0f81ba5de21599bcb6de1f4d4b0
benWhen <159172786+benWhen@users.noreply.github.com>
2024-04-14 19:38:05 +0000

    separated edit info functions

--- ResumeBuilderApp/urls.py
+++ ResumeBuilderApp/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('editor/', views.editor, name='editor'),
     path('saveResume/', views.saveResume, name='saveResume'),
     path('loadResume/', views.loadResume, name='loadResume'),
-    path('quickResume/<int:user_id>', views.quickResume, name='quickResume')
+    path('quickResume/<int:user_id>', views.quickResume, name='quickResume'),
+    path('add_Skill/', views.add_Skill, name='add_Skill'),
+    path('add_Job/', views.add_Job, name='add_Job'),
+    path('add_Education/', views.add_Education, name='add_Education'),
     ]
 #comment so I can re-push
